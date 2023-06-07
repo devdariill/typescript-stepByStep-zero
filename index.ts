d661903eed4ce17a8d0af278b0754c1b02ae0a28
nATOr <gowluonade@gmail.com>
2023-06-07 13:27:10 -0500

    functions

--- index.ts
+++ index.ts
@@ -13,3 +13,23 @@ function fn2({a}:{a:number}):void {
 //   const {name} = a
 //   console.log(name)
 // }
+
+const fn4 = (fn: (name:string)=> string|void)=>{
+  fn('hello')
+}
+const sayHi = (name:string) => {
+  console.log(`hello ${name}`)
+  return 'hello'
+}
+fn4(sayHi) 
+
+// const fn4 = (fn: Function )=>{
+//   fn('hello')
+// }
+// const sayHi = (name:string) => {
+//   console.log(`hello ${name}`)
+// }
+// fn4(sayHi) 
+
+const sumar = (a:number, b:number):number => a + b
+const restar: (a:number, b:number) => number = (a,b) => a - b
